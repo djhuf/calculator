37d49432f0565e8002ba902d0f29492e9ba12030
Pavel <pashap420@gmail.com>
2022-08-24 21:02:47 +0300

    version1.0

---
+++ calcul.py
@@ -0,0 +1,32 @@
+def sum(a, b):
+    print(a + b)
+
+
+def sub(a, b):
+    print(a - b)
+
+
+def mult(a, b):
+    print(a * b)
+
+
+def div(a, b):
+    if b != 0:
+        print(a / b)
+    else:
+        print("Ділення на нуль неможливе")
+
+
+a = int(input("Введіть перше число: "))
+b = int(input("Введіть друге число: "))
+symbol = input("Введіть математичний знак (+, -, /, *): ")
+if symbol == "+":
+    sum(a, b)
+elif symbol == "-":
+    sub(a, b)
+elif symbol == "/":
+    div(a, b)
+elif symbol == "*":
+    mult(a, b)
+else:
+    print("Ви ввели неправильний математичний знак.")
